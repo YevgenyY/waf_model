04fe2d1ee18f6a5a3ba1d3dcf37b0e52531c22a9
Yevgeny Y <yy@ixi.ru>
2016-09-08 17:34:50 +0300

    Find time delay between clicks.

--- waf_model.R
+++ waf_model.R
@@ -1,11 +1,29 @@
+setwd('~/work/grcc/waf_model')
+
 df <- read.csv('data/log.csv',header = FALSE, stringsAsFactors = F)
 names(df) <- c('ip', 'time', 'method', 'url')
 
 Sys.setlocale("LC_TIME", "en_US.UTF-8")  # Modern Linux etc.
 df$time <- strptime(df$time, "%d/%b/%Y:%H:%M:%S")
+df$ip <- factor(df$ip)
 
-# unique ip and url
-uip <- unique(df$ip)
-uurl <- unique((tolower(df$url)))
+# split dataframe by ip adresses
+X <- split(df, df$ip)
+Y <- X
+
+# Count time diff for each click of each IP
+z <- lapply(Y, function(x) abs(c(0,diff(x$time, lag=1))))
+
+R <- mapply(c, Y, Z, SIMPLIFY = FALSE)
+
+# Find time delays between clicks
+Z <- X[[150]]
+Z$diff <- abs(c(0,diff(Z$time, lag=1)))
+ggplot(Z, aes(x=diff)) + geom_histogram()
+
+ggplot(Z, aes(x=diff)) + geom_density()
+
+
+
 
 
