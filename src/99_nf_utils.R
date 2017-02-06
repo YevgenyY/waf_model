a97d4cba2534e16ae606f52033049d61a26ac58c
YevgenyY <yy@ixi.ru>
2017-02-06 16:57:19 +0300

    Add netflow utils

--- src/99_nf_utils.R
+++ src/99_nf_utils.R
@@ -1,10 +1,23 @@
 
-top100_pkts_num <- function(traf, threshold) {
-  # Find champions by pkts
-  threshold <- 100
-  x <- aggregate(trafIn$flow_pkts, by=list(trafIn$dst_ip), FUN=sum)
-  names(x) <- c('ip', 'value'); x <- x[order(x$value, decreasing = TRUE),]
-  x <- x[x$value > threshold,]
+top100_ip <- function(x, y, threshold = 100, fun=sum) {
+  # Find top100 ip addresses by packets number or octets
+  #
+  # Args:
+  #   x: Dataframe
+  #   names(df)
+  #     [1] "src_ip"      "src_port"    "dst_ip"      "dst_port"    
+  #     "flow_octets" "flow_pkts"   "ip_proto" 
+  #   y: 0 - packets number
+  #      1 - octets
+  #   threshold: number to remove IP addresses with low-rate of packets/octets 
   
-  return(x)
+  if (y == 0) {
+    z <- aggregate(x$flow_pkts, by=list(x$dst_ip), FUN=fun)
+  } else {
+    z <- aggregate(x$flow_pkts, by=list(x$dst_ip), FUN=fun)
+  }
+  names(z) <- c('ip', 'value'); z <- z[order(z$value, decreasing = TRUE),]
+  z <- z[z$value > threshold,]
+  
+  return(z)
 }
