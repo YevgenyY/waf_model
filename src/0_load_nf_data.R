f5c124967a13f43a4d2a4d4c61a530ebfcd0e771
YevgenyY <yy@ixi.ru>
2017-02-06 12:24:40 +0300

    Start analysing netflow data

---
+++ src/0_load_nf_data.R
@@ -0,0 +1,11 @@
+setwd('~/work/netflow')
+
+bulk <- read.csv('data/netflow.log', sep=',')
+names(bulk) <- c('src_ip','src_port','dst_ip','dst_port',
+                 'in_pkts','out_pkts', 'tcp_in_pkts', 'tcp_out_pkts',
+                 'tcp_syn_in_pkts', 'tcp_syn_out_pkts', 'tcp_in_bytes', 'tcp_out_bytes',
+                 'udp_in_pkts', 'udp_out_pkts', 'udp_in_bytes', 'udp_out_bytes',
+                 'icmp_in_pkts', 'icmp_out_pkts', 'icmp_in_bytes', 'icmp_out_bytes',
+                 'in_flows', 'out_flows'
+                 )
+save(bulk, file='data/bulk.Rda')
