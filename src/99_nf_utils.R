f6116a67dc327502e169d23c2741f93e1bb50c27
YevgenyY <yy@ixi.ru>
2017-02-06 17:12:33 +0300

    Fix top100_ip function

--- src/99_nf_utils.R
+++ src/99_nf_utils.R
@@ -7,15 +7,11 @@ top100_ip <- function(x, y, threshold = 100, fun=sum) {
   #   names(df)
   #     [1] "src_ip"      "src_port"    "dst_ip"      "dst_port"    
   #     "flow_octets" "flow_pkts"   "ip_proto" 
-  #   y: 0 - packets number
-  #      1 - octets
+  #   y: data frame column number for aggregation
   #   threshold: number to remove IP addresses with low-rate of packets/octets 
   
-  if (y == 0) {
-    z <- aggregate(x$flow_pkts, by=list(x$dst_ip), FUN=fun)
-  } else {
-    z <- aggregate(x$flow_pkts, by=list(x$dst_ip), FUN=fun)
-  }
+  z <- aggregate(x[,y], by=list(x$dst_ip), FUN=fun)
+  
   names(z) <- c('ip', 'value'); z <- z[order(z$value, decreasing = TRUE),]
   z <- z[z$value > threshold,]
   
