f0113615f777458fb951a89cc3e77624b64333ec
YevgenyY <yy@ixi.ru>
2017-02-06 18:21:49 +0300

    Add calculation of mean / median for each ip.

--- src/99_nf_utils.R
+++ src/99_nf_utils.R
@@ -9,6 +9,7 @@ top100_ip <- function(x, y, threshold = 100, fun=sum) {
   #     "flow_octets" "flow_pkts"   "ip_proto" 
   #   y: data frame column number for aggregation
   #   threshold: number to remove IP addresses with low-rate of packets/octets 
+  #   fun: aggregation function
   
   z <- aggregate(x[,y], by=list(x$dst_ip), FUN=fun)
   
@@ -16,4 +17,22 @@ top100_ip <- function(x, y, threshold = 100, fun=sum) {
   z <- z[z$value > threshold,]
   
   return(z)
+}
+
+aggregate_ip <- function(x, y, fun=sum) {
+  # Aggregate ip addresses featuress
+  #
+  # Args:
+  #   x: Dataframe
+  #   names(df)
+  #     [1] "src_ip"      "src_port"    "dst_ip"      "dst_port"    
+  #     "flow_octets" "flow_pkts"   "ip_proto" 
+  #   y: data frame column number for aggregation
+  #   threshold: number to remove IP addresses with low-rate of packets/octets 
+  #   fun: aggregation function
+  
+  z <- aggregate(x[,y], by=list(x$dst_ip), FUN=fun)
+  names(z) <- c('ip', 'value')
+  
+  return(z$value)
 }
